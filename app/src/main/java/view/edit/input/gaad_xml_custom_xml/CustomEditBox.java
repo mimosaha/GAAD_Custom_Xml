26f8729c0c75f9f3076921d1fc28c2328416bffb
Mimo <mimosaha@gmail.com>
2018-07-06 18:40:10 +0600

    add custom edit box

--- app/src/main/java/view/edit/input/gaad_xml_custom_xml/CustomEditBox.java
+++ app/src/main/java/view/edit/input/gaad_xml_custom_xml/CustomEditBox.java
@@ -3,17 +3,24 @@ package view.edit.input.gaad_xml_custom_xml;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.TintTypedArray;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by Mimo on 1/29/2018.
@@ -22,7 +29,11 @@ import android.widget.TextView;
 public class CustomEditBox extends LinearLayout {
 
     private EditText inputEditBox;
+
+    private RelativeLayout bottomArea;
+
     private TextView countTextBox;
+    private TextView inputTextBox;
     private View underlineEditBox;
 
     private int unFocusedColor = R.color.colorPrimary,
@@ -30,6 +41,9 @@ public class CustomEditBox extends LinearLayout {
             errorColor = R.color.colorAccent;
 
     private int editTextCount = 0, errorMaxLength;
+    private String inputString;
+    private int inputTextColor;
+    private float inputTextSize;
     private Context context;
 
     @SuppressLint("RestrictedApi")
@@ -44,12 +58,20 @@ public class CustomEditBox extends LinearLayout {
         setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 
         editTextCount = tintTypedArray.getInt(R.styleable.CustomEditBoxEliment_inputCounts, 0);
+        inputString = tintTypedArray.getString(R.styleable.CustomEditBoxEliment_inputText);
+        inputTextSize = tintTypedArray.getDimension(R.styleable.CustomEditBoxEliment_inputTextSize, 0);
+        inputTextColor = tintTypedArray.getColor(R.styleable.CustomEditBoxEliment_inputTextColor, 0);
+
         if (editTextCount > 0) {
             errorMaxLength = (int) (editTextCount * 0.9);
         }
 
         inputEditBox = new EditText(context);
+
+        bottomArea = new RelativeLayout(context);
+
         countTextBox = new TextView(context);
+        inputTextBox = new TextView(context);
         underlineEditBox = new View(context);
 
         initAllViews();
@@ -65,11 +87,44 @@ public class CustomEditBox extends LinearLayout {
 
         underlineEditBox.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 2));
 
-        if (editTextCount > 0) {
-            addView(countTextBox);
+        if (editTextCount > 0 || !TextUtils.isEmpty(inputString)) {
+            operateBottomArea();
         }
 
         unFocusView();
+    }
+
+    private void operateBottomArea() {
+        Random random = new Random();
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT);
+
+        bottomArea.setLayoutParams(layoutParams);
+
+        RelativeLayout.LayoutParams paramsInput = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams paramsCount = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+
+        countTextBox.setId(random.nextInt());
+        inputTextBox.setId(random.nextInt());
+
+        paramsInput.addRule(RelativeLayout.ALIGN_PARENT_LEFT, inputTextBox.getId());
+        paramsInput.addRule(RelativeLayout.LEFT_OF, countTextBox.getId());
+
+        paramsCount.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, countTextBox.getId());
+        paramsCount.setMargins(10, 0, 0, 0);
+
+        if (editTextCount > 0) {
+            bottomArea.addView(countTextBox, paramsCount);
+        }
+
+        if (!TextUtils.isEmpty(inputString)) {
+            bottomArea.addView(inputTextBox, paramsInput);
+        }
+
+        addView(bottomArea);
     }
 
     private void controlFocusView() {
@@ -128,6 +183,22 @@ public class CustomEditBox extends LinearLayout {
         if (editTextCount > 0) {
             countTextBox.setText("0/" + editTextCount);
             countTextBox.setGravity(Gravity.RIGHT);
+        }
+
+        if (TextUtils.isEmpty(inputString)) {
+            inputString = "Hello";
+        }
+
+        inputTextBox.setText(inputString);
+        inputTextBox.setMaxLines(1);
+        inputTextBox.setEllipsize(TextUtils.TruncateAt.END);
+
+        if (inputTextColor != 0) {
+            inputTextBox.setTextColor(inputTextColor);
+        }
+
+        if (inputTextSize > 0) {
+            inputTextBox.setTextSize(TypedValue.COMPLEX_UNIT_PX, inputTextSize);
         }
     }
 
